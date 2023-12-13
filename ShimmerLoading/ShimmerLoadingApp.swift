bf9983f94775000bb63c48c07eac47d8d3f7e72e
Luka Vujnovac <84441240+lukavujnovac@users.noreply.github.com>
2023-12-13 13:16:16 +0100

    Initial Commit

---
+++ ShimmerLoading/ShimmerLoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerLoadingApp.swift
+//  ShimmerLoading
+//
+//  Created by Luka Vujnovac on 13.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct ShimmerLoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
